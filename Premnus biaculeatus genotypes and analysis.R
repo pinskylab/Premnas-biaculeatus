6698818380b87921af138b0e79996db2b9da67a6
Kyra-Fitz <74556116+Kyra-Fitz@users.noreply.github.com>
2020-11-23 12:47:46 -0500

    Calculating pairwise Fst between populations

--- Premnus biaculeatus genotypes and analysis.R
+++ Premnus biaculeatus genotypes and analysis.R
@@ -1,6 +1,16 @@
 #Premnas biaculeatus Genotypes and Analysis
 
+library(genepop)
+
 #Read in data
 
-prbi_fulldata = read.csv("PRBI_GenAlEx_2009-11-12.csv")
+prbi_genalex = read.csv("PRBI_GenAlEx_2009-11-12.csv")
+prbi_genepop = read.csv("PRBI_2009-09-07.txt")
 
+#Calculate pairwise Fst between populations
+#Unsure on pairs= and sizes= arguments- what does True and False mean for each?
+
+Fst("PRBI_2009-09-07.txt", pairs = TRUE, outputFile = "prbi_genepop_fst.txt", 
+    dataType = "Diploid", verbose = interactive())
+
+#Resulting Fst matrices found in "prbi_genepop_fst.txt" and "prbi_genepop_fst.txt.MIG" 
