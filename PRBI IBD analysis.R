94456a7501131cc8be028c1c9c90d2b31f35f7be
Kyra-Fitz <74556116+Kyra-Fitz@users.noreply.github.com>
2020-12-17 17:30:33 -0500

    Updated geographic distance matrix

--- PRBI IBD analysis.R
+++ PRBI IBD analysis.R
@@ -17,28 +17,42 @@ test_HW("PRBI_2009-09-07.txt", outputFile="prbi_hw.txt")
 ##Calculate pairwise Fst between populations
 #pairs=TRUE to get a pairwise Fst matrix
 
-Fst("PRBI_2009-09-07.txt", pairs = TRUE, outputFile = "prbi_genepop_fst.txt", 
+Fst("PRBI_2009-09-07.txt", pairs = TRUE, outputFile = "prbi_genepop_fst.csv", 
     dataType = "Diploid", verbose = interactive())
 
-#Resulting Fst matrices found in "prbi_genepop_fst.txt" and "prbi_genepop_fst.txt.MIG" 
+#Resulting Fst matrices found in "prbi_genepop_fst.csv" and "prbi_genepop_fst.csv.MIG" 
 
 
 ##Calculate geographic distances between populations using geodist package
 
 install.packages("geodist")
 library(geodist)
+install.packages("tidyverse")
+library(tidyverse)
 
-#Create vector for longitude and latitude of populations
+#Import avg. lat and long from csv file
+
+prbi_avglatlong <- read.csv("PRBI_AvgLatLong.csv")
+
+#Rename and reorder columns
+prbi_avglatlong <- rename(prbi_avglatlong, latitude = Avg..Latitude, 
+                                longitude = Avg..Longitude)
+
+prbi_avglatlong <- prbi_avglatlong[c("longitude", "latitude", "Population")]
+
+prbi_avglatlong_matrix <- as.matrix(prbi_avglatlong)
+
+#Use geodist function to calculate distances (output is in meters)
+#Getting error- says it can't determine longitude and latitude columns
+prbi_distance_meters <- geodist(prbi_avglatlong, measure="geodesic")
+
+#Create vector for longitude and latitude of populations from prbi_avglatlong
 #Listed in sequential order of populations from pop 1-pop 22
 
-prbi_long <- c(124.2489589, 124.177941, 123.3179946, 123.4794616, 
-               123.5785325, 123.6545571, 123.8037213, 124.0285911,
-               124.0109535, 124.032865, 125.0251015, 124.6560577)
+prbi_long <- prbi_avglatlong$longitude
 prbi_long
 
-prbi_lat <- c(10.22834459, 10.17666235, 9.414770978, 9.616869822,
-              9.848377696, 10.07330622, 10.22512305, 10.48777602,
-              10.76495538, 10.9440459, 10.01649883, 10.54725344)
+prbi_lat <- prbi_avglatlong$latitude
 prbi_lat
 
 #Use geodist_vec to calculate pairwise distances
@@ -52,6 +66,39 @@ prbi_geodistance_meters
 
 prbi_geodistance_km <- prbi_geodistance_meters/1000
 prbi_geodistance_km
+
+#Relabel matrix columns and rows to be Populations 1-22
+rownames(prbi_geodistance_km) <- c(1, 2, 7, 8, 9, 10, 11, 13, 14, 15, 19, 22)
+colnames(prbi_geodistance_km) <- c(1, 2, 7, 8, 9, 10, 11, 13, 14, 15, 19, 22)
+prbi_geodistance_km
+
+#Remove diagonals/upper triangle of the geographic distance matrix
+prbi_geodistance_km[upper.tri(prbi_geodistance_km, diag=T)] = NA
+
+#Load in Fst matrix
+prbi_fst <- read.csv("prbi_genepop_fst.csv.MIG")
+prbi_fst <- as.matrix(prbi_fst)
+
+##Linearize Fst and Plot Linear Fst vs. Geographic Distance
+
+#Read in Fst and Geographic Distance Matrices
+#These ones only have information below the diagonal
+#Need to use as.matrix to move this to matrix form (otherwise geo distance is not in a matrix)
+
+prbi_fst_trimatrix = read.csv("Pairwise Fst Triangle Matrix.csv")
+prbi_geodistance_trimatrix = read.csv("Geographic Distance Triangle Matrix.csv")
+
+
+#Linearize Fst (Fst/(1-Fst))
+prbi_fstlin = as.matrix(prbi_fst_trimatrix/(1-prbi_fst_trimatrix))
+
+#Plot Linear Fst vs. Geographic Distance
+plot(prbi_geodistance_trimatrix, prbi_fstlin)
+
+
+
+
+
 
 ##Create a matrix for pairwise Fst
 
@@ -82,18 +129,6 @@ ibd("PRBI_2009-09-07.txt", geoDistFile="prbi_genepop_geomatrix.txt",
     testPoint=0, mantelPermutations=1000, verbose = interactive())
 
 
-##Linearize Fst and Plot Linear Fst vs. Geographic Distance
 
-#Read in Fst and Geographic Distance Matrices
-#These ones only have information below the diagonal
-
-prbi_fst_trimatrix = read.csv("Pairwise Fst Triangle Matrix.csv")
-prbi_geodistance_trimatrix = read.csv("Geographic Distance Triangle Matrix.csv")
-
-#Linearize Fst (Fst/(1-Fst))
-prbi_fstlin = as.matrix(prbi_fst_trimatrix/(1-prbi_fst_trimatrix))
-
-#Plot Linear Fst vs. Geographic Distance
-plot(c(prbi_geodistance_trimatrix), c(prbi_fstlin))
 
 
